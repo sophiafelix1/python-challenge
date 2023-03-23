1aa7ae6036faf19771063f4bd404b00e0668bcb9
sophiafelix1 <117216181+sophiafelix1@users.noreply.github.com>
2023-03-23 12:14:43 -0700

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,39 @@
 #You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
 import os 
 import csv
-election_data = os.path.join("election_data.csv")
 #Figure out amount of candidates and how many votes each candidate received
 candidates = []
-Candidate_Vote = []
+votes_per_candidate = {}
 #Out of the total votes of the election what was the percentage of votes per candidate
 Total_Votes = 0
-Percent_Votes = []
+election_data = os.path.join("election_data.csv")
 with open(election_data, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvreader)
 #Loop through data
     for row in csvreader:
-        candidates = candidates + 1
-#List of candidates and add each vote per candidate
-    if(row[2] not in Candidate_Vote):
-        Candidate_Vote.append(row[2])
-        Total_Votes.append(0)
-candidatesIndex = candidates.index (row[2])
-Total_Votes[candidatesIndex] += 1
+        Total_Votes += 1
+        Name_Candidate = row[2]
+        if Name_Candidate not in candidates:
+            candidates.append(Name_Candidate)
+            votes_per_candidate[Name_Candidate] = 0
+        votes_per_candidate[Name_Candidate] += 1
+#percentages of vote per candidate
+percent_votes = {}
+for candidates, votes in votes_per_candidate.items():
+    percent_votes[candidates] = (votes /Total_Votes) * 100
+
+Winner =max(votes_per_candidate, key=votes_per_candidate.get)
 #Print the results 
 print("Election Results")
 print("-------------------------")
 print(f" Total Votes : {Total_Votes}")
 print("-------------------------")
+for candidates, vote in votes_per_candidate.items():
+    print(f"{candidates}: {percent_votes[candidates]:.3f}% ({votes})")
+print("-------------------")
+print(f"Winner: {Winner}")
+print("------------------------")
 #A complete list of candidates who received votes
 
 #The percentage of votes each candidate won
@@ -32,5 +41,3 @@ print("-------------------------")
 #The total number of votes each candidate won
 
 #The winner of the election based on popular vote
-
-
